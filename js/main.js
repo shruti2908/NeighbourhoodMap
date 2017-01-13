8154f27864b1b9863bd9ca27871abd3a41514726
shruti2908 <shrutiupreti.nmims@gmail.com>
2017-01-13 13:50:19 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -17,106 +17,124 @@ var marker;
 var markers= [];
 
 
-//Contains all the locations in myLocations(Model)
+//Contains all the locations in myLocations (Model)
 var myLocations = [
   { 
     name: 'Law Garden',
     location:{lat: 23.026473,lng: 72.560664},
     address:'Netaji Road, Ellisbridge, Maharashtra Society, Ellisbridge, Ahmedabad, Gujarat 380009',
-    phone: "No Contact Information"
+    phone: "No Contact Information",
+    url: 'No Website'
   },
 
   { 
     name: 'Parimal Garden' ,
     location:{lat: 23.020212, lng: 72.556125},
     address:'Parimal Cross Road, Ambawadi, Panchavati Society, Ambawadi, Ahmedabad, Gujarat 380006',
-    phone: "No Contact Information"
+    phone: "No Contact Information",
+    url: 'No Website'
   },
   { 
     name: 'KFC CG Road',
     location:{lat:23.021579, lng:72.557739},
     address:'No 2, 3rd Eye Building, Chimanlal Girdharlal Road, Opp Parimal Garden, Navrangpura, Ellisbridge, Ahmedabad, Gujarat 380009',
-    phone: 07940099900
+    phone: 07940099900,
+    url: 'https://www.kfc.co.in/menuwow.php'
   },
   { 
     name: 'CEPT University',
     location:{lat: 23.038159, lng:72.549927},
     address:'Kasturba Lalabhai Campus, University Road, Vasant Vihar, Navrangpura, University Area, Ahmedabad, Gujarat 380009',
-  	phone: 07926302470
+  	phone: 07926302470,
+  	url: 'http://www.cept.ac.in/'
   },
 
   { 
     name: 'Jalaram Chachh',
     location:{lat: 23.036117,lng: 72.563533},
     address:'Kimsim Complex, Near Swastik Chaurastha, Navrangpura Road, Navrangpura, Shrimali Society, Navrangpura, Ahmedabad, Gujarat 380009',
- 	phone: 09512577449
+ 	phone: 09512577449,
+ 	url: 'No Website'
   },
 
   { 
     name: 'Shiv Cinema',
     location: {lat: 23.031782, lng: 72.571531},
     address:'Opp. City Gold Cinema, Near Atma House, Ashram Road, Vishalpur, Muslim Society, Navrangpura, Ahmedabad, Gujarat 380015',
-    phone: 07933016909
+    phone: 07933016909,
+    url: 'https://www.pvrcinemas.com/;jsessionid=EEE9BC89A61E184381E31D354ADA177A'
   },
 
   { 
     name: 'Passport Seva Kendra',
     location: {lat: 23.032738,lng: 72.566246},
     address:'Ground & First Floor, Arya Arcade, Netaji Road, Near Mithakhali, Navrangpura, Mithakhali, Navrangpura, Ahmedabad, Gujarat 380009',
-    phone: 07926309107
+    phone: 07926309107,
+    url: 'http://passportindia.gov.in/AppOnlineProject/welcomeLink'
   },
   
   { 
     name: 'Four Points By Sheraton',
     location: {lat: 23.022579, lng: 72.568913},
     address:'Opposite Gujarat College, Ellis Bridge, Ellisbridge, Ahmedabad, Gujarat 380006',
-    phone: 07930912345
+    phone: 07930912345,
+    url: 'http://www.starwoodhotels.com/fourpoints/property/overview/index.html?propertyID=3927'
   },
   
   { 
     name: 'Central',
     location: {lat: 23.020925, lng: 72.555271},
     address: 'C.G. ROAD, KOLONNADE CENTRE, OPP. SAFFRON TOWER,AMBAVADI, Panchavati Society, Ambawadi, Ahmedabad, Gujarat 380006',
-    phone: 07930481777
+    phone: 07930481777,
+    url: 'No Website'
   },
 
   { 
     name: 'National Handloom',
     location: {lat: 23.028264, lng: 72.558973},
     address:'Near Mansarovar Apartment, Chimanlal Girdharlal Rd, Ellisbridge, Ahmedabad, Gujarat 380009',
-    phone: 07926403474
+    phone: 07926403474,
+    url: 'No Website'
   },
 
   { 
    name: 'Jade Blue',
    location: {lat:23.0285, lng: 72.5585},
    address:'B-61,, Pariseema Complex, Chimanlal Girdharlal Road, Ellisbridge, Ahmedabad, Gujarat 380006',
-   phone: 07926468182
+   phone: 07926468182,
+   url: 'https://jadeblue.com/'
   },
 
   { 
    name: 'Alfa Bazaar',
    location: {lat: 23.025017, lng: 72.559373},
    address:'Alpha Bazzar , Opposite Thakorebhai Desai Hall, Law Garden, Netaji Rd, Ellisbridge, Ahmedabad, Gujarat 380006',
-   phone: 09227888002
+   phone: 09227888002,
+   url: 'No Website'
   },
+
   { 
    name: 'Radisson Blu Hotel',
    location: {lat: 23.0233, lng: 72.5569},
    address:'Near Panchvati Cross Roads, Off C.G. Road, Ambawadi, Ellisbridge, Ahmedabad, Gujarat 380006',
-   phone: 07940501234
+   phone: 07940501234,
+   url: 'https://www.radissonblu.com/en/hotel-ahmedabad'
   },
+
   { 
    name: 'State Bank of India',
    location: {lat: 23.028723, lng: 72.560972},
    address:'Netaji Rd, Sardar Patel Nagar Cross Road, Opp Law Garden, Ellisbridge, Ellisbridge, Ahmedabad, Gujarat 380009',
-   phone: 18004253800
+   phone: 18004253800,
+   url: 'https://www.sbi.co.in/'
   },
+
   { 
    name: 'Marks and Spencer',
    location: {lat: 23.0244736, lng: 72.5543167},
    address:'AlphaOne City Center, C G Square Mall, Near Panchvati Circle, CG Road, AlphaOne City Center, Ellisbridge, Ahmedabad, Gujarat 380009',
-   phone: 08511143250
+   phone: 08511143250,
+   url: 'https://www.marksandspencer.com/'
   }
 
 ];
@@ -137,6 +155,7 @@ function initializeMap() {
             title: myLocations[i].name,
             address: myLocations[i].address,
             phone: myLocations[i].phone,
+            url: myLocations[i].url,
             animation: google.maps.Animation.BOUNCE,
             draggable: true
         }); 
@@ -160,7 +179,7 @@ function initializeMap() {
   
   //Displays markers on the listed location
   function displayMarkerList () {
-    map.setCenter({lat: 42.150897, lng: -77.079306});
+  	
           for (var i = 0; i < markers.length; i++) {
             markers[i].setMap(map);
             bounds.extend(markers[i].position);
@@ -191,12 +210,16 @@ function populateInfoWindow(marker, info_window) {
   $.ajax({
     url: url_Wikipedia,
     dataType: "jsonp",
-    success: function( response ) {
-      var article = response[2];
-      var link = response[3];
+    success: function( wikiResponse ) {
+      var article = wikiResponse[2];
+      var link = wikiResponse[3];
       if(info_window.marker != marker) {
+      	//Sets content for information window
             info_window.marker = marker;
-              info_window.setContent('<h1><strong><a href="' + link +'">'+ marker.title +'</a></strong></h1>' + '<strong>' + "Address:" + '</strong>' + '<p><em>'+ marker.address + '<p><strong>' + "Contact: " + '</strong></p>' + '<p>' + marker.phone + '</p>' + '</em></p>'+'<div class="article">' + article + '</div><br>'+'<img class="wiki_img" src="' + streetViewUrl + '">');
+              info_window.setContent('<h1><strong><a href="' + link +'">'+ marker.title +'</a></strong></h1>' + '<strong>' + "Address:" 
+              + '</strong>' + '<p><em>'+ marker.address + '<p><strong>' + "Contact: " + '</strong></p>' + '<p>' + marker.phone + '</p>'
+              + '</em></p>' + '<p><strong>' + "Website: " + '</strong></p>' + '<a href="' + marker.url +'">' + marker.url + '</a>' + '<div class="article">' +
+              article + '</div><br>'+'<img class="wiki_img" src="' + streetViewUrl + '">');
               info_window.open(map, marker);
 
               // Checks if marker property is cleared
